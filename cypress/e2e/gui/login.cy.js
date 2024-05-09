c4bde294538c448deb3eec3617cd78d2f51593b3
bsicchieri <brunosicchieri@hotmail.com>
2024-05-08 21:46:21 -0300

    Add tests

---
+++ cypress/e2e/gui/login.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="Cypress" />
+
+describe('Login', () => {
+  it('Successfully', () => {
+    const user = Cypress.env('user_name')
+    const password = Cypress.env('user_password')
+    const options = { cacheSession: false }
+
+    cy.login(user, password, options)
+
+    cy.get('.qa-user-avatar').should('be.visible')
+  })
+})
+
---
+++ cypress/e2e/gui/logout.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="Cypress" />
+
+describe('Logout', () => {
+  beforeEach(() => {
+    cy.login()
+    cy.visit('/')
+  })
+  
+  it('Successfully', () => {
+    cy.logout()
+  
+    cy.url().should('be.equal', `${Cypress.config('baseUrl')}/users/sign_in`)
+  })
+})
